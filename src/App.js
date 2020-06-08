7f371ae80ad4e45cf850eba573bfd71dac4966d9
Muhaamd-Haseeb <64950545+Muhaamd-Haseeb@users.noreply.github.com>
2020-06-09 02:18:07 +0500

    function Reuseability
    I practice a function reuseability with JXS

--- src/App.js
+++ src/App.js
@@ -1,13 +1,41 @@
 import React from 'react';
-
-import './App.css';
-
-function App() {
-  return (
+import logo from './logo.svg';
+function Hi(){
+ return(
    <div>
-     <h1>Hello</h1>
-   </div>
-  );
-}
+     <strong>
+       Hello  World
+     </strong>
+     <h1>Muhammad Ahmad</h1>
+     <ul>
+       <li>Muhammad Abubakar</li>
+       <li>Muhammad                Umer</li>
+       <li>Muhammad Usman</li>
+       <li>Muhammad Ali</li>
+       {5+10}
+       <App  name="haseeb"age="12"/>
+       <MediaCard p={"Hello everyone"} h1={"now passing h1"} sorce={logo}/>
 
-export default App;
+
+     </ul>
+   </div>
+ );
+}
+let App=({name,age})=>(
+     <div>
+       <h1>{name}</h1>
+        <h1>{age}</h1> 
+
+     </div>
+  );
+
+  let MediaCard=({p,h1,sorce})=>(
+      <div>
+        <p>{p}</p>
+  <h1>{h1}</h1>
+  <img src={sorce} alt="Haseeb"/>
+      </div>
+  )
+  
+
+export default Hi;
