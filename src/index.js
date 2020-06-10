124a7b7e0c9ba709c94c4742d2fc2663f5e3c335
Muhaamd-Haseeb <64950545+Muhaamd-Haseeb@users.noreply.github.com>
2020-06-10 11:11:24 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './App';
-import Temperature from './temperatur';
+
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App/>,document.getElementById('root'));
